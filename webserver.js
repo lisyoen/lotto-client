fce25270cab6193a0ed8308b83d73c39620cfa49
lisyoen <lisyoen@gmail.com>
2023-04-11 22:31:13 +0900

    post API

--- webserver.js
+++ webserver.js
@@ -1,10 +1,2 @@
 var http = require('http');
 var fs = require('fs');
-
-var port = 4000;
-http.createServer(function (req, res) {
-  var file = fs.readFileSync('test.html');
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(file);
-}).listen(port);
-console.log("Web Server start. port:", port);
